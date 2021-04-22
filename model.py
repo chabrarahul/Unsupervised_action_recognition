0e5eb36e3b60d3403518725e100df9e549c22ae3
Rahul Jain <40121060+chabrarahul@users.noreply.github.com>
2021-04-22 17:05:22 -0400

    Create model.py

---
+++ model.py
@@ -0,0 +1,26 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
+
+from modules import *
+
+
+class Model(nn.Module)
+
+    def __init__(self, input_size, drop_prob=0.2):
+         super(Model, self).__init__()
+         self.transformer_encoder = TransformerEncoderPartSeq()
+         self.encoder = LstmNet() # number of layer should be given 
+         self.auto_encoder = AutoEncoderDecoder()
+         self.decoder = LstmNet() # one layer lstm network as the decoder
+
+    
+    def forward(self, input): 
+         output = self.transformer(input)
+         output = self.encoder(output)
+         out_for_prediction, output = self.auto_encoder(output)
+         output = self.decoder(output)
+
+         return output_for_prediction, output  
